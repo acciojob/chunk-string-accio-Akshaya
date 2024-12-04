acdb7acf8413be2757a877ffefd1c190d25a9835
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-04 06:41:35 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function stringChop(str, size) {
   // your code here
-	if (!str || typeof str !== "string") return [];
-	if (!size || typeof size !== "number" || size <=0){
-		throw new Error("size must be a positive number.");
-	}
+  if (!str || typeof str !== "string") return [];
+  if (!size || typeof size !== "number" || size <=0){
+    throw new Error("size must be a positive number.");
+  }
 
-	const chunks = [];
+  const chunks = [];
 
-	for(let i = 0; i < str.length; i += size){
-		size.push(str.slice(i,i + size));
-	}
-	return chunks;
+  for(let i = 0; i < str.length; i += size){
+    chunks.push(str.slice(i,i + size));
+  }
+  return chunks;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+const size = Number(prompt("Enter Chunk Size.")); // Convert string to number
 alert(stringChop(str, size));
